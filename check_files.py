fdee761b456229e99518fbf6243113a32d631a0d
Atharvsyo <atharvasawalakhe@gmail.com>
2025-10-30 23:00:01 +0530

    Update check_files.py

--- check_files.py
+++ check_files.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyDRsvt4NKLx7tjXzpxdsTvzjjPkpBZu8Jc")
+genai.configure(api_key="")
 
 models = genai.list_models()
 for m in models:
